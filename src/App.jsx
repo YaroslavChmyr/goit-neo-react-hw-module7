3fbc8ed09bdcc91ea8613529ba4ab58138d8b476
YaroslavChmyr <yaroslav.ch72@gmail.com>
2024-10-12 13:10:37 +0300

    Task completed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchContacts } from "./redux/contactsOps";
+import { selectIsLoading, selectError } from "./redux/contactSlice.js";
 import css from "./App.module.css";
 
 const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   return (
     <div className={css.container}>
       <h1 className={css.title}>Phonebook</h1>
       <ContactForm />
       <SearchBox />
+      {isLoading && !error && <h2>Loading...</h2>}
       <ContactList />
     </div>
   );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-
 import { contactReducer } from "./contactSlice";
 import { filtersReducer } from "./filtersSlice";
 
-const persistConfig = {
-  key: 'contacts',
-  storage,
-  blacklist: ['filters'],
-};
-
-const persistedContactsReducer = persistReducer(persistConfig, contactReducer);
-
 export const store = configureStore({
   reducer: {
-    contacts: persistedContactsReducer,
+    contacts: contactReducer,
     filters: filtersReducer,
   },
 });
-
-export const persistor = persistStore(store);
